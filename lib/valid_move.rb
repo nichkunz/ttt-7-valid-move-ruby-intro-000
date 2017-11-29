3408721047387350eccab9b4347f8ced4dac632d
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-29 15:52:40 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 def valid_move?(board, index)
   if index > 0 && index < 9
-    if position_taken?(board, index) == false
+    if position_taken? == false
       return true
     else
       return false
